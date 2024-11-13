9d8b647ea04b19c7f1720da997a945f8ba39125d
theavitw <190320132009.ict.avit.desai@gmail.com>
2024-11-13 12:33:20 +0530

    Updates

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
