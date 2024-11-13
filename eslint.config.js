054436d3b4415ae5a557f934762acd35db3fdd62
theavitw <190320132009.ict.avit.desai@gmail.com>
2024-11-13 13:11:17 +0530

    asdadsasd

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,27 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        console: true,
+        process: true,
+      },
+    },
+    rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }], // Allow console.warn and console.error
+      'no-unused-vars': ['off', {
+        vars: 'all', // Check all
+        args: 'after-used', // Ignore unused variables after they are used
+        ignoreRestSiblings: false, // Ignore unused variables that are rest siblings
+        varsIgnorePattern: '^_', // Ignore unused variables starting with an underscore
+        argsIgnorePattern: '^_', // Ignore unused variables starting with an underscore
+      }], // Warn on unused variables
+      'no-undef': ['off',{
+        typeof: true,
+      }], // Error on undefined variables
+      'quotes': ['error', 'single', { allowTemplateLiterals: true }], // Use single quotes
+    },
+  },
 ];
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // main.js
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
-import cors from "cors";
-import bcrypt from "bcrypt";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import { otpGenerate } from "./utils/helper.js";
-import { emailOTP } from "./utils/emailuTILS.js";
+import cors from 'cors';
+import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import { otpGenerate } from './utils/helper.js';
+import { emailOTP } from './utils/emailuTILS.js';
 
 dotenv.config();
 
@@ -16,7 +16,7 @@ const port = 8080;
 const prisma = new PrismaClient();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: 'http://localhost:5173',
   optionsSuccessStatus: 200,
 };
 
@@ -25,11 +25,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 // User Registration
-app.post("/register", async (req, res) => {
+app.post('/register', async (req, res) => {
   try {
     const { name, email, password, id: googleToken } = req.body;
     if (!name || !email || (!password && !googleToken)) {
-      return res.status(400).send("Missing required fields");
+      return res.status(400).send('Missing required fields');
     }
 
     // Check if the email already exists
@@ -37,7 +37,7 @@ app.post("/register", async (req, res) => {
       where: { email },
     });
     if (existingUser) {
-      return res.status(400).send("Email already exists");
+      return res.status(400).send('Email already exists');
     }
 
     // Hash the password if provided
@@ -48,7 +48,7 @@ app.post("/register", async (req, res) => {
     const otp = (await otpGenerate()).toString();
 
     if (!otp) {
-      return res.status(500).send("Failed to generate OTP");
+      return res.status(500).send('Failed to generate OTP');
     }
 
     // const otpExpiry = new Date(new Date().getTime() + 10 * 60 * 1000);
@@ -57,43 +57,43 @@ app.post("/register", async (req, res) => {
     if (!otpSent) {
       return res
         .status(400)
-        .json({ status: false, message: "Failed to send OTP" });
+        .json({ status: false, message: 'Failed to send OTP' });
     }
     if (!otpSent) {
       return res.status(400).json({
         status: false,
-        message: "Invalid Email Address",
+        message: 'Invalid Email Address',
       });
     }
-    return res.status(200).json({
-      status: true,
-      message: "User registered successfully, Please check your email for OTP.",
-    });
 
-    // Create a new user in PostgreSQL using Prisma
     await prisma.user.create({
       data: {
         name,
         email,
         password: hashedPassword,
-        googleToken: googleToken || "",
+        googleToken: googleToken || '',
         otp,
       },
     });
 
-    res.send("User registered successfully");
+    return res.status(200).json({
+      status: true,
+      message: 'User registered successfully, Please check your email for OTP.',
+    });
+  
+    res.send('User registered successfully');
   } catch (error) {
-    console.error("Error registering user:", error);
-    res.status(500).send("Failed to register user");
+    console.error('Error registering user:', error);
+    res.status(500).send('Failed to register user');
   }
 });
 
 // User Login
-app.post("/login", async (req, res) => {
+app.post('/login', async (req, res) => {
   try {
     const { email, password, id: googleToken } = req.body;
     if (!email || (!password && !googleToken)) {
-      return res.status(400).send({ string: "Missing required fields" });
+      return res.status(400).send({ string: 'Missing required fields' });
     }
 
     // Fetch user by email using Prisma
@@ -102,41 +102,41 @@ app.post("/login", async (req, res) => {
     });
 
     if (!user) {
-      return res.status(404).send({ string: "Email not found" });
+      return res.status(404).send({ string: 'Email not found' });
     }
 
     if (!googleToken && password) {
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
-        return res.status(401).send({ string: "Incorrect password" });
+        return res.status(401).send({ string: 'Incorrect password' });
       } else {
         res.status(200).send({
-          string: "Logged in successfully",
+          string: 'Logged in successfully',
           token: user.password,
           user,
         });
       }
     } else if (googleToken && googleToken === user.googleToken) {
       res.status(200).send({
-        string: "Logged in successfully",
+        string: 'Logged in successfully',
         token: user.googleToken,
         user,
       });
     } else {
-      return res.status(401).send({ string: "Kindly Login With Password" });
+      return res.status(401).send({ string: 'Kindly Login With Password' });
     }
   } catch (error) {
-    console.error("Error logging in:", error);
-    res.status(500).send({ string: "Failed to login" });
+    console.error('Error logging in:', error);
+    res.status(500).send({ string: 'Failed to login' });
   }
 });
 
 // validate otp
-app.post("/validate-otp", async (req, res) => {
+app.post('/validate-otp', async (req, res) => {
   try {
     const { email, otp } = req.body;
     if (!email || !otp) {
-      return res.status(400).send({ string: "Missing required fields" });
+      return res.status(400).send({ string: 'Missing required fields' });
     }
 
     // Fetch user by email using Prisma
@@ -145,24 +145,24 @@ app.post("/validate-otp", async (req, res) => {
     });
 
     if (!user) {
-      return res.status(404).send({ string: "Email not found" });
+      return res.status(404).send({ string: 'Email not found' });
     }
 
     if (user.otp === otp) {
-      res.status(200).send({ string: "OTP validated successfully" });
+      res.status(200).send({ string: 'OTP validated successfully' });
     } else {
-      return res.status(401).send({ string: "Invalid OTP" });
+      return res.status(401).send({ string: 'Invalid OTP' });
     }
   } catch (error) {
-    console.error("Error validating OTP:", error);
-    res.status(500).send({ string: "Failed to validate OTP" });
+    console.error('Error validating OTP:', error);
+    res.status(500).send({ string: 'Failed to validate OTP' });
   }
 });
 
 // User Logout
-app.post("/logout", async (req, res) => {
+app.post('/logout', async (req, res) => {
   try {
-    res.clearCookie("token");
+    res.clearCookie('token');
     const { email, cart } = req.body;
 
     if (email) {
@@ -173,14 +173,15 @@ app.post("/logout", async (req, res) => {
       });
     }
 
-    res.send({ string: "Logged out successfully" });
+    res.send({ string: 'Logged out successfully' });
   } catch (error) {
-    console.error("Error logging out:", error);
-    res.status(500).send({ string: "Failed to logout" });
+    console.error('Error logging out:', error);
+    res.status(500).send({ string: 'Failed to logout' });
   }
 });
 
 // Start the server
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Example app listening at http://localhost:${port}`);
 });
